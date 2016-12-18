b364d8dc27962e93eae923f834780fa10b0b54ee
Jannick Garthen <jannick.garthen@gmail.com>
2016-12-18 21:35:37 +0100

    Clean up and add new icons

--- public/script.js
+++ public/script.js
@@ -1,110 +1,9 @@
-// (function() {
-
-// 	'use strict';
-
-// 	var _switchTimer;
-
-// 	var _buttonDown = false;
-// 	document.addEventListener('touchstart', function(ev) {
-// 		ev.preventDefault();
-// // 
-// 		ev.target.classList.add('touch-active');
-
-// 		if(ev.target.nodeName === 'BUTTON') {
-// 			_buttonDown = true;
-// 			_switchTimer = setTimeout(function() {
-// 				ev.target.parentNode.classList.add('switch');
-// 				window.navigator.vibrate(50);
-// 				removeTouchActive();
-// 			}, 1000);
-// 		} else {
-// 			removeSwitch();
-// 		}
-
-// 	}, false);
-
-// 	document.addEventListener('touchend', function(ev) {
-// 		ev.target.classList.add('touch-end');
-// 		setTimeout(removeTouchActive, 100);
-// 		setTimeout(removeTouchEnd, 300);
-// 		clearTimeout(_switchTimer);
-
-// 		_buttonDown = false;
-// 		if(ev.target.nodeName === 'BUTTON') {
-// 			api.send(ev.target.getAttribute('data-device'), ev.target.value);
-// 			ev.target.classList.add('touch-active');
-// 		}
-
-// 	});
-
-// 	var api = (function() {
-
-// 		var socketUrl = location.origin; // location.protocol + '//' + location.host
-// 		var socket    = io.connect(socketUrl);
-
-// 		socket.on('update', function(state) {
-// 			var keys = Object.keys(state);
-// 			keys.forEach(function(key) {
-// 				var value = state[key];
-// 				var activeSelector = '[data-device="' + key + '"][value="' + value + '"]';
-// 				var inactiveSelector = '[data-device="' + key + '"]:not([value="' + value + '"])';
-// 				var active = document.querySelector(activeSelector);
-// 				var inactive = document.querySelector(inactiveSelector);
-// 				active.classList.add('is-active');
-// 				inactive.classList.add('is-inactive');
-// 				active.classList.remove('is-inactive');
-// 				inactive.classList.remove('is-active');
-// 			});
-
-// 		});
-
-// 		return {
-// 			send: function (device, state) {
-
-// 				socket.emit('toggleState', {
-// 					unitCode: JSON.parse(device),
-// 					state: state
-// 				});
-// 			},
-
-// 			on: function() {
-// 				return socket.on.apply(this, arguments);
-// 			}
-
-// 		};
-
-// 	})();
-
-// 	var removeTouchActive = function() {
-// 		var el = document.querySelector('.touch-active');
-// 		if(el) { el.classList.remove('touch-active'); }
-// 	};
-
-// 	var removeTouchEnd = function() {
-// 		var el = document.querySelector('.touch-end');
-// 		if(el) { el.classList.remove('touch-end'); }
-// 	};
-
-// 	var removeSwitch = function() {
-// 		[].splice.call(document.querySelectorAll('.switch'), 0).forEach(function(el) {
-// 			el.classList.remove('switch');
-// 		});
-// 	};
-
-// })();
-
-
 (function() {
-
 	'use strict';
-
 	var api = (function() {
-
 		var socket;
-
 		return {
 			send: function (device, state) {
-
 				socket.emit('toggleState', {
 					unitCode: JSON.parse(device),
 					state: JSON.parse(state)
@@ -139,9 +38,7 @@
 			isConnected: function() {
 				return socket.connected;
 			}
-
 		};
-
 	})();
 
 	api.connect();
@@ -161,10 +58,9 @@
 				el.classList.remove('is-active');
 			} else {
 				el.value = 0;
-				el.classList.add('is-active');				
+				el.classList.add('is-active');
 			}
 		});
-
 	});
 
 	document.addEventListener('click', function(ev) {
@@ -177,9 +73,5 @@
 			window.navigator.vibrate(25);
 		}
 		api.send(el.getAttribute('data-device'), el.value);
-
 	});
-
-	window.api = api;
-
 })();
--- server.js
+++ server.js
@@ -1,80 +1,61 @@
-var express = require('express');
-var http    = require('http');
-var swig    = require('swig');
-var exec    = require('child_process').exec;
+const express = require('express');
+const exec = require('child_process').exec;
 
-var app     = express();
-var server  = require('http').Server(app);
-var io      = require('socket.io')(server);
-var elro_wiringpi = function(options, cb) {
-	exec('sudo ' + __dirname + '/elro_wiringpi.py ' + options.join(' '), cb);
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const elroWiringpi = (options, cb) => {
+  exec(`sudo ${__dirname}/elro_wiringpi.py ${options.join(' ')}`, cb);
 };
 
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+const isProd = app.get('env') === 'production';
 
-var isProd = app.get('env') === 'production';
-
-app.set('view cache', isProd);
-swig.setDefaults({ cache: isProd });
-
-if(isProd) {
-	io.enable('browser client minification');
-	io.enable('browser client etag');
-	io.enable('browser client gzip');
-	io.set('log level', 1);
-	io.set('transports', [
-		'websocket',
-		'flashsocket',
-		'htmlfile',
-		'xhr-polling',
-		'jsonp-polling'
-	]);
+if (isProd) {
+  io.enable('browser client minification');
+  io.enable('browser client etag');
+  io.enable('browser client gzip');
+  io.set('log level', 1);
+  io.set('transports', [
+    'websocket',
+    'flashsocket',
+    'htmlfile',
+    'xhr-polling',
+    'jsonp-polling'
+  ]);
 }
 
-app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res) {
-	res.render('index', {
-		isProd: app.get('env') === 'production'
-	});
+app.use(express.static(`${__dirname}/public`));
+// app.get('/', (req, res) => {
+//   res.render('index');
+// });
+
+const _state = {};
+const _toString = Object.prototype.toString;
+io.sockets.on('connection', (socket) => {
+  socket.emit('update', _state);
+  socket.on('toggleState', function _toggleState(data) {
+    if (_toString.call(data.unitCode) === '[object Array]') {
+      let timeout = 0;
+      const stateIsArray = _toString.call(data.state) === '[object Array]';
+      data.unitCode.forEach((unitCode, key) => {
+        setTimeout(_toggleState, timeout, {
+          unitCode,
+          state: stateIsArray ? data.state[key] : data.state,
+        });
+        timeout += 500;
+      });
+      return;
+    }
+    const unitCode = Math.pow(2, data.unitCode);
+    const state = parseInt(data.state, 10);
+
+    elroWiringpi([unitCode, state], () => {
+      _state[data.unitCode] = data.state;
+      io.sockets.emit('update', _state);
+    });
+  });
 });
 
-var _state = {};
-var _toString = Object.prototype.toString;
-io.sockets.on('connection', function(socket) {
-	socket.emit('update', _state);
-	socket.on('toggleState', function _toggleState(data) {
-
-		if(_toString.call(data.unitCode) === '[object Array]') {
-			var timeout = 0;
-			var stateIsArray = _toString.call(data.state) === '[object Array]';
-			data.unitCode.forEach(function(unitCode, key) {
-
-				setTimeout(_toggleState, timeout, {
-					unitCode: unitCode,
-					state: stateIsArray ? data.state[key] : data.state
-				});
-				timeout += 500;
-
-			});
-			return;
-
-		} else {
-
-			var unitCode = Math.pow(2, data.unitCode);
-			var state    = parseInt(data.state, 10);
-
-			elro_wiringpi([unitCode, state], function() {
-				_state[data.unitCode] = data.state;
-				io.sockets.emit('update', _state);
-			});
-
-		}
-
-	});
-});
-
-var port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 server.listen(port);
 console.log('Express server listening on port %d in %s mode', port, app.get('env'));
