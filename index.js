bf4be61ca69eab90f56da77fd1aaa67fe96555e7
Jannick Garthen <jannick.garthen@gmail.com>
2017-05-05 12:03:02 +0200

    Move loose logic into middleware

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import 'babel-polyfill';
 
-import startServer, { sockets } from './lib/server';
+import startServer from './lib/server';
 import createStore from './lib/server/store';
-import { activate, deactivate } from './lib/server/actions/devicePowerPoint';
 import { setDateTime } from './lib/server/actions/date';
-import { add as addSocket, remove as removeSocket } from './lib/server/actions/socket';
 
 const store = createStore();
 
@@ -12,20 +10,5 @@ const store = createStore();
   store.dispatch(setDateTime(Date.now()));
   setTimeout(tick, 1000);
 }());
-
-sockets.on('connection', (socket) => {
-  store.dispatch(addSocket(socket));
-  socket.on('device:mutate', ({ unitCode, state }) => {
-    const action = state ? activate : deactivate;
-    if (Array.isArray(unitCode)) {
-      unitCode.forEach(code => store.dispatch(action(code)));
-    } else {
-      store.dispatch(action(unitCode));
-    }
-  });
-  socket.on('disconnect', () => {
-    store.dispatch(removeSocket(socket));
-  });
-});
 
 startServer();
