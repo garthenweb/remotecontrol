75eeb81e5430ce8a1486995997d66c45380132fc
Jannick Garthen <jannick.garthen@gmail.com>
2017-05-20 15:37:19 +0200

    Introducing tickInterval for locals

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import 'babel-polyfill';
 import startServer from './lib/server';
 import createStore from './lib/server/store';
 import { setDateTime } from './lib/server/actions/date';
+import locals from './locals.json';
 
 const store = createStore();
 
 (function tick() {
   store.dispatch(setDateTime(Date.now()));
-  setTimeout(tick, 1000);
+  setTimeout(tick, locals.tickInterval);
 }());
 
 startServer();
