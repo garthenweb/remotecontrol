5c3685e782a4646de6771c057eaeeb299702504e
Jannick Garthen <jannick.garthen@gmail.com>
2018-01-02 22:29:19 +0100

    improve error message for missing locals.json

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import 'babel-polyfill';
+import fs from 'fs';
 
 import startServer from './lib/server';
 import createStore from './lib/server/store';
 import { setDateTime } from './lib/server/actions/date';
+
+try {
+  fs.accessSync('./localss.json', fs.constants.F_OK);
+} catch (exc) {
+  console.error('`locals.json` is not accessible, this is most likely because the file was not created. Please check wether it exists and is accessible!');
+  process.exit(1);
+}
+
 import locals from './locals.json';
 
 const store = createStore();
