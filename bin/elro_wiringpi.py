cebd1715824a3619dba6667a644fa649ac1b396c
Jannick Garthen <jannick.garthen@gmail.com>
2018-01-03 23:51:12 +0100

    fix indentation

--- bin/elro_wiringpi.py
+++ bin/elro_wiringpi.py
@@ -38,85 +38,82 @@ import wiringpi
 import json
 
 class RemoteSwitch(object):
-   repeat = 10 # Number of transmissions
-   pulselength = 300 # microseconds
-   counter = 0
+  repeat = 10 # Number of transmissions
+  pulselength = 300 # microseconds
+  counter = 0
 
-   def __init__(self, unit_code, system_code=[1,1,1,1,1], pin=17):
-      '''
-      devices: A = 1, B = 2, C = 4, D = 8, E = 16
-      system_code: according to dipswitches on your Elro receivers
-      pin: according to Broadcom pin naming
-      '''
-      self.pin = pin
-      self.system_code = system_code
-      self.unit_code = unit_code
+  def __init__(self, unit_code, system_code=[1,1,1,1,1], pin=17):
+    '''
+    devices: A = 1, B = 2, C = 4, D = 8, E = 16
+    system_code: according to dipswitches on your Elro receivers
+    pin: according to Broadcom pin naming
+    '''
+    self.pin = pin
+    self.system_code = system_code
+    self.unit_code = unit_code
 
-   def switchOn(self):
-      self._switch(wiringpi.HIGH)
+  def switchOn(self):
+    self._switch(wiringpi.HIGH)
 
-   def switchOff(self):
-      self._switch(wiringpi.LOW)
+  def switchOff(self):
+    self._switch(wiringpi.LOW)
 
-   def _switch(self, switch):
-      self.bit = [142, 142, 142, 142, 142, 142, 142, 142, 142, 142, 142, 136, 128, 0, 0, 0]
+  def _switch(self, switch):
+    self.bit = [142, 142, 142, 142, 142, 142, 142, 142, 142, 142, 142, 136, 128, 0, 0, 0]
 
-      for t in range(5):
-         if self.system_code[t]:
-            self.bit[t]=136
-      x=1
-      for i in range(1,6):
-         if self.unit_code & x > 0:
-            self.bit[4+i] = 136
-         x = x<<1
+    for t in range(5):
+        if self.system_code[t]:
+          self.bit[t]=136
+    x=1
+    for i in range(1,6):
+        if self.unit_code & x > 0:
+          self.bit[4+i] = 136
+        x = x<<1
 
-      if switch == wiringpi.HIGH:
-         self.bit[10] = 136
-         self.bit[11] = 142
+    if switch == wiringpi.HIGH:
+        self.bit[10] = 136
+        self.bit[11] = 142
 
-      bangs = []
-      for y in range(16):
-         x = 128
-         for i in range(1,9):
-            b = (self.bit[y] & x > 0) and wiringpi.HIGH or wiringpi.LOW
-            bangs.append(b)
-            x = x>>1
+    bangs = []
+    for y in range(16):
+        x = 128
+        for i in range(1,9):
+          b = (self.bit[y] & x > 0) and wiringpi.HIGH or wiringpi.LOW
+          bangs.append(b)
+          x = x>>1
 
-      wiringpi.wiringPiSetupSys()
-      print "pin %s" % (self.pin)
-      print "system code %s" % (self.system_code)
-      print "mode %s" % (wiringpi.OUTPUT)
-      print "write first %s" % (wiringpi.LOW)
-      wiringpi.pinMode(self.pin,wiringpi.OUTPUT)
-      wiringpi.digitalWrite(self.pin,wiringpi.LOW)
+    wiringpi.wiringPiSetupSys()
+    print "pin %s" % (self.pin)
+    print "system code %s" % (self.system_code)
+    print "mode %s" % (wiringpi.OUTPUT)
+    print "write first %s" % (wiringpi.LOW)
+    wiringpi.pinMode(self.pin,wiringpi.OUTPUT)
+    wiringpi.digitalWrite(self.pin,wiringpi.LOW)
 
-      startTime = time.time()
-      for z in range(self.repeat):
-         for b in bangs:
-            wiringpi.digitalWrite(self.pin, b)
-            # self.counter = self.counter + 1
-            # print "write %s" % (self.counter)
-            time.sleep(self.pulselength/1000000.)
-      print (time.time() - startTime) * 1000
+    startTime = time.time()
+    for z in range(self.repeat):
+      for b in bangs:
+        wiringpi.digitalWrite(self.pin, b)
+        time.sleep(self.pulselength/1000000.)
+    print (time.time() - startTime) * 1000
 
 if __name__ == '__main__':
-   import sys
+  import sys
 
-   # change the pin accpording to your wiring
-   default_pin = 17
+  # change the pin accpording to your wiring
+  default_pin = 17
 
-   if len(sys.argv) < 3:
-      print "usage: python %s int_unit_code int_state (e.g. '%s 2 1' switches unit 2 on)" % \
-         (sys.argv[0], sys.argv[0])
-      sys.exit(1)
+  if len(sys.argv) < 3:
+    print "usage: python %s int_unit_code int_state (e.g. '%s 2 1' switches unit 2 on)" % (sys.argv[0], sys.argv[0])
+    sys.exit(1)
 
-   device = RemoteSwitch(
-     unit_code = int(sys.argv[1]),
-     system_code = json.loads(sys.argv[2]),
-     pin = default_pin
-   )
+  device = RemoteSwitch(
+    unit_code = int(sys.argv[1]),
+    system_code = json.loads(sys.argv[2]),
+    pin = default_pin
+  )
 
-   if int(sys.argv[3]):
-      device.switchOn()
-   else:
-      device.switchOff()
+  if int(sys.argv[3]):
+    device.switchOn()
+  else:
+    device.switchOff()
