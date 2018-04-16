0004d12d2ad106c8d8f879cdbc403b05a586733f
Jannick Garthen <jannick.garthen@gmail.com>
2018-04-16 22:08:53 +0200

    fix(*): wrong font family

--- react/src/index.js
+++ react/src/index.js
@@ -5,7 +5,6 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
