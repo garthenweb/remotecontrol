751b8cca66144ae10568230bf39eb5a005f68f8d
Jannick Garthen <jannick.garthen@gmail.com>
2018-04-22 22:16:06 +0200

    feat(control): add touch feedback on button click

--- react/src/App.js
+++ react/src/App.js
@@ -4,17 +4,37 @@ import Button from './components/Button';
 import deviceActions from './actions/device';
 import './App.css';
 
+const buttonClickFeedback = () => {
+  if (typeof window.navigator.vibrate === 'function') {
+    window.navigator.vibrate(25);
+  }
+};
+
 function App({ devices, powerOn, powerOff, powerToggle }) {
   const deviceIds = Object.keys(devices);
+
+  const onButtonOnClick = () => {
+    buttonClickFeedback();
+    powerOn(deviceIds);
+  };
+  const onButtonOffClick = () => {
+    buttonClickFeedback();
+    powerOff(deviceIds);
+  };
+  const onDeviceClick = (id) => () => {
+    buttonClickFeedback();
+    powerToggle(id);
+  };
+
   return [
     <div key="group1" className="btn-group">
-      <Button theme="on" onClick={() => powerOn(deviceIds)} content="Turn on" />
-      <Button theme="off" onClick={() => powerOff(deviceIds)} content="Shut off" />
+      <Button theme="on" onClick={onButtonOnClick} content="Turn on" />
+      <Button theme="off" onClick={onButtonOffClick} content="Shut off" />
     </div>,
     ...Object.values(devices).map(device => (
       <Button
         key={device.id}
-        onClick={() => powerToggle(device.id)}
+        onClick={onDeviceClick(device.id)}
         content={device.name}
         isActive={device.getSerializedStateOf('power', Boolean)}
       />
