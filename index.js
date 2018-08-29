6793ac1e7a9ceea5f053c3202fb8151ed1a66559
Jannick Garthen <jannick.garthen@gmail.com>
2018-08-29 13:36:10 +0200

    chore(*): improve error message for locals.json

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { setLocation } from './lib/server/actions/user';
 try {
   fs.accessSync('./locals.json', fs.constants.F_OK);
 } catch (exc) {
-  console.error('`locals.json` is not accessible, this is most likely because the file was not created. Please check wether it exists and is accessible!');
+  console.error('`locals.json` is not accessible. The file needs to be created by hand and contains configuration about the devices you want to control. In case it exists in the root folder of this project, please check whether the access rights are granted for the same user you are executing the script with. Please see the documentation for more information.');
   process.exit(1);
 }
 
