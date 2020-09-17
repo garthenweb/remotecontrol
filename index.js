0caee880a6eecea0d8392c4b8af7c5713e06c4a9
Jannick Garthen <jannick.garthen@gmail.com>
2020-09-17 15:50:25 +0200

    fix: human readable locals.json warning was not executed

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 import 'babel-polyfill';
 import fs from 'fs';
-
-import startServer from './lib/server';
-import createStore from './lib/server/store';
-import { setDateTime } from './lib/server/actions/date';
-import { setLocation } from './lib/server/actions/user';
+import path from "path";
 
 try {
-  fs.accessSync('./locals.json', fs.constants.F_OK);
+  fs.accessSync(path.join(__dirname, "./locals.json"), fs.constants.F_OK);
+  require("./lib/main");
 } catch (exc) {
-  console.error('`locals.json` is not accessible. The file needs to be created by hand and contains configuration about the devices you want to control. In case it exists in the root folder of this project, please check whether the access rights are granted for the same user you are executing the script with. Please see the documentation for more information.');
+  console.error(
+    "`locals.json` is not accessible. The file needs to be created by hand and contains configuration about the devices you want to control. In case it exists in the root folder of this project, please check whether the access rights are granted for the same user you are executing the script with. Please see the documentation for more information."
+  );
   process.exit(1);
 }
-
-import locals from './locals.json';
-
-const store = createStore();
-
-(function tick() {
-  store.dispatch(setDateTime(Date.now()));
-  setTimeout(tick, locals.tickInterval);
-}());
-
-startServer(process.env.PORT || 3001);
